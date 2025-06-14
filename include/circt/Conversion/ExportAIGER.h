95ee9229ccc66de9bc5a8df1207d13790bf3bba6
Hideto Ueno <uenoku.tokotoko@gmail.com>
2025-06-14 02:53:27 -0700

    save

--- include/circt/Conversion/ExportAIGER.h
+++ include/circt/Conversion/ExportAIGER.h
@@ -23,24 +23,27 @@ class TimingScope;
 } // namespace mlir
 
 namespace circt {
+namespace hw {
+class HWModuleOp;
+} // namespace hw
 
 /// Options for AIGER export.
 struct ExportAIGEROptions {
   /// Whether to export in binary format (aig) or ASCII format (aag).
   /// Default is ASCII format.
   bool binaryFormat = false;
-  
+
   /// Whether to include symbol table in the output.
   /// Default is true.
   bool includeSymbolTable = true;
-  
+
   /// Whether to include comments in the output.
   /// Default is true.
   bool includeComments = true;
 };
 
 /// Export an MLIR module containing AIG dialect operations to AIGER format.
-mlir::LogicalResult exportAIGER(mlir::ModuleOp module, llvm::raw_ostream &os,
+mlir::LogicalResult exportAIGER(hw::HWModuleOp module, llvm::raw_ostream &os,
                                 const ExportAIGEROptions *options = nullptr);
 
 /// Register the `export-aiger` MLIR translation.
